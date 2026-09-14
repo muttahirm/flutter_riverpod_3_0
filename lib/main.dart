f4520b3b2f3e3204857a46d0fdfb41b803c88ae4
Muttahir Mumtaz <mumtaz.muttahir@gmail.com>
2026-09-14 04:45:09 +0200

    adds home screen to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_3_0/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,34 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('Start', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
