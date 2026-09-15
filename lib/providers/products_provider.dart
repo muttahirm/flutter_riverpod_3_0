1c312ca0c57d8cd1e9c26918e28a40e459b53209
Muttahir Mumtaz <mumtaz.muttahir@gmail.com>
2026-09-15 05:15:45 +0200

    Cleans the code

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_3_0/models/product.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
